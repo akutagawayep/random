301225f8e4e84c46cda3bae2a6940a5e7e6dcd00
akutagawayep <108138348+akutagawayep@users.noreply.github.com>
2022-10-10 17:42:43 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 let box = document.querySelector(".box");
+
+const num1=document.querySelector(".num1")
+const num2=document.querySelector(".num2")
+
 const btn = document.querySelector(".btn");
 
+
 btn.addEventListener("click", () => {
-  return (box.innerHTML = Math.floor(Math.random() * (600 - 100)) + 100);
+  return (box.innerHTML = Math.floor(Math.random() * (`${num1.value}` - `${num2.value}`)) );
 });
+
+
